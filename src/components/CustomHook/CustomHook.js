9a303628d95036ab9e20dbd6632be6778cfd619f
mobile-web <wepiohip@gmail.com>
2021-05-04 12:55:38 +0700

    up

--- src/components/CustomHook/CustomHook.js
+++ src/components/CustomHook/CustomHook.js
@@ -787,6 +787,38 @@ const GetUsersButton = () => {
 }
         `
     },
+    {
+        id:19,
+        name:"useStateWithPromise",
+        htmlEl : `
+        const useStateWithPromise = (initialState) => {
+            const [state, setState] = useState(initialState);
+            const resolverRef = useRef(null);
+          
+            useEffect(() => {
+              if (resolverRef.current) {
+                resolverRef.current(state);
+                resolverRef.current = null;
+              }
+              /**
+               * Since a state update could be triggered with the exact same state again,
+               * it's not enough to specify state as the only dependency of this useEffect.
+               * That's why resolverRef.current is also a dependency, because it will guarantee,
+               * that handleSetState was called in previous render
+               */
+            }, [resolverRef.current, state]);
+          
+            const handleSetState = useCallback((stateAction) => {
+              setState(stateAction);
+              return new Promise(resolve => {
+                resolverRef.current = resolve;
+              });
+            }, [setState])
+          
+            return [state, handleSetState];
+          };
+        `
+    },
 
 ]
 
@@ -866,10 +898,10 @@ function CustomHook() {
                 </textarea>
             </div>
 
-            <CodeMirror
+            {/* <CodeMirror
             value={code123}
             options={options}
-            />
+            /> */}
         </div>
     );
 }
