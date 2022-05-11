bca3ed039e8d5fb96ce8093c1952338d65cc76ed
huydz <wepiohip@gmail.com>
2022-05-11 14:00:28 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,36 @@
-import './app.scss';
+import "./app.scss";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { pages as pagesTemp } from "./pages";
 
-
 function App() {
-
-  const pages = pagesTemp.sort(function(a,b){
+  const pages = pagesTemp.sort(function (a, b) {
     return a.text.localeCompare(b.text);
-  })
- 
+  });
+
   return (
     <Router>
       <div className="App">
-          <div className="navbar">
-            <nav>
-              <ul>
-                {
-                  pages.map(page =>(
-                    <Link to={ page.to }>
-                      <li>
-                        { page.text }
-                      </li>
-                    </Link>
-
-                  ))
-                }
-              </ul>
-            </nav>
-          </div>
-          <div className="container">
-             <Switch>
-              {
-                pages.map(page =>(
-                  <Route path={ page.to } exact>
-                    {
-                      page.comp
-                    }
-                  </Route>
-                ))
-              }
-            </Switch>
-          </div>
+        <div className="navbar">
+          <nav>
+            <ul>
+              {pages.map((page) => (
+                <Link to={page.to}>
+                  <li>{page.text}</li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className="container">
+          <Switch>
+            {pages.map((page) => (
+              <Route path={page.to} exact>
+                {page.comp}
+              </Route>
+            ))}
+          </Switch>
+        </div>
       </div>
     </Router>
   );
